7f87d30052fcef14603982322948b03f3596b16a
Cathy Lill <cathygail.lill@gmail.com>
2016-04-19 23:03:36 +1000

    added theme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
-  theme = require('./bespoke-theme-webrtc.min'),
+  theme = require('./theme-webrtc.min'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
